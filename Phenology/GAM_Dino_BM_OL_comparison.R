c7d96c7a299b89112ce641816a43318cb1a91d54
Victor Pochic <138684342+vpochic@users.noreply.github.com>
2025-04-01 13:27:00 +0200

    Update GAM_Dino_BM_OL

--- Phenology/GAM_Dino_BM_OL_comparison.R
+++ Phenology/GAM_Dino_BM_OL_comparison.R
@@ -1,6 +1,8 @@
 ### A Dinophysis GAM at Basse Michaud ###
 # V. POCHIC - 2024-11-20
 
+# Updated 2025-04-01 to get a proper GAM of Dinophysis at Basse Michaud
+
 
 #### Packages ####
 library(tidyverse)
@@ -213,6 +215,162 @@ ggplot() +
   # Text
   labs(title = 'Basse Michaud', x = 'Date', y = 'Dinophysis cells observed in 10 mL') +
   theme_classic()
+
+#### Confidence intervals ####
+
+# We can try to obtain better confidence intervals.
+# This section is almost entirely taken from a post by Gavin Simpson on his
+# blog 'From the bottom of the heap'
+# Reference : 
+# https://fromthebottomoftheheap.net/2016/12/15/simultaneous-interval-revisited/
+
+# We want to define better confidence intervals and plot some examples of
+# model fits taken from the bayesian posterior distribution of the model
+# to illustrate model variability
+
+# First
+# A function for generating random values from a multivariate normal
+rmvn <- function(n, mu, sig) { ## MVN random deviates
+  L <- mroot(sig)
+  m <- ncol(L)
+  t(mu + L %*% matrix(rnorm(m*n), m, n))
+}
+
+# Select only Basse Michaud site
+Season_Dino_BM_select <- filter(Season_Dino_BM, Code_point_Libelle == 'Basse Michaud')
+
+# Save this file
+# write.csv2(Season_Dino_BM_select, 'Season_Dino_BM_20250401.csv', row.names = FALSE,
+#            fileEncoding = 'ISO-8859-1')
+
+
+# We extract a few things from the fitted gam
+
+# Bayesian covariance matrix (unconditional = TRUE means we adjust for the 
+# smoothing parameters being estimated rather than known values)
+Vb <- vcov(gam_Dino_BM)
+# New data
+newd <- with(Season_Dino_BM_select, data.frame(Day = rep(seq(1, 365, length = 365),
+                                               length(unique(Year)))
+                                        ))
+
+newd <- group_by(newd, Day) %>%
+  arrange(Day, by_group = TRUE)
+
+# Add the Year vector
+newd$Year <- rep(unique(Season_Dino_BM_select$Year), 
+                 365)
+
+newd <- group_by(newd, Year, Day) %>%
+  arrange(Year, by_group = TRUE)
+
+# Prediction by the model on the ***link*** scale, on the new data
+pred <- predict(gam_Dino_BM, newd, se.fit = TRUE, type = 'link')
+# Isolate standard error of the predicted fit
+se.fit <- pred$se.fit
+
+# Set the pseudo-random seed to make results reproducible (?)
+set.seed(42)
+# specify the number of simulations to generate
+N <- 5000
+
+# N draws from a multivariate normal distributed matrix with mean 0 (?)
+BUdiff <- rmvn(N, mu = rep(0, nrow(Vb)), sig = Vb)
+
+# Remove
+rm(Vb)
+
+# Calculating a function now (?)
+Cg <- predict(gam_Dino_BM, newd, type = "lpmatrix")
+simDev <- Cg %*% t(BUdiff)
+
+# Remove on the way to free memory space
+rm(Cg)
+rm(BUdiff)
+
+### CAREFUL ! The 'absdev' and 'masd' steps can take a lot of time, especially
+# when N is large. Don't panic, it eventually finishes running after a few tens
+# of minutes.
+
+# Finding the absolute values of the standardized deviation from the true model (?)
+absDev <- abs(sweep(simDev, 1, se.fit, FUN = "/"))
+rm(simDev)
+
+# Computing the maximum of the absolute standard deviation
+masd <- apply(absDev, 2L, max)
+rm(absDev)
+
+# Finding the critical value used to scale the standard errors to yield 
+# the simultaneous interval
+crit <- quantile(masd, prob = 0.95, type = 8)
+
+# Now that crit is calculated, remove the big masd table that takes all the 
+# memory space
+rm(masd)
+
+# It now runs much quicker. Nice.
+
+# Adjusting the variables in the prediction to take
+pred <- transform(cbind(data.frame(pred), newd),
+                  uprP = fit + (2 * se.fit),
+                  lwrP = fit - (2 * se.fit),
+                  # The simultaneous CI is based on the critical value
+                  # calculated just above
+                  uprS = fit + (crit * se.fit),
+                  lwrS = fit - (crit * se.fit))
+
+# Calculating response_pred -> the gam fitted on every year on the repsonse
+# scale
+response_pred <- pred %>%
+  mutate(across(c('fit', 'lwrS', 'uprS', 'lwrP', 'uprP'), ~ ilink(.)))
+
+# Save response_pred
+# write.csv2(response_pred, 'response_pred_GAMDino_BM_20250401.csv', row.names = FALSE,
+#            fileEncoding = 'ISO-8859-1')
+
+# Constructing a CI based on maximum and minimum simultaneous interval for
+# plotting
+pred_plot <- pred %>%
+  group_by(Day) %>%
+  summarise(
+    # median
+    median.fit = median(fit),
+    # Confidence intervals
+    # Here we take minimum and maximum to encompass all possible years (17), as
+    # the prediction varies among years
+    # Simultaneous confidence interval (CI)
+    lwrS = min(lwrS), uprS = max(uprS),
+    # Point-wise CI
+    lwrP = min(lwrP), uprP = max(uprP), 
+    .groups = 'keep')
+
+# Saving pred_plot so we don't have to re-run the model every time
+# write.csv2(pred_plot, 'pred_plot_20241120_16sites.csv', row.names = FALSE,
+#            fileEncoding = "ISO-8859-1")
+
+# Transform values so they are expressed in the response variable units
+# (by applying the inverse link function)
+response_pred_plot <- as.data.frame(pred_plot) %>%
+  mutate(across(c('median.fit', 'lwrS', 'uprS', 'lwrP', 'uprP'), ~ ilink(.)))
+
+# And save that shall we
+# write.csv2(response_pred_plot, 'response_pred_plot_20241120_16sites.csv',
+#            row.names = FALSE, fileEncoding = 'ISO-8859-1')
+
+# You can check that the backtransformation by the inverse link function
+# worked by comparing the pred_plot and response_pred_plot tables.
+# The response_pred_plot values should be higher.
+
+# Checking the confidence intervals
+# minimum and maximum values
+min(response_pred_plot$median.fit)
+min(response_pred_plot$lwrS) # Not < 0, nice :)
+min(response_pred_plot$lwrP) # Not < 0, nice :)
+max(response_pred_plot$median.fit)
+max(response_pred_plot$uprS)
+max(response_pred_plot$uprP)
+# We can see that the simultaneous interval is slightly wider than the
+# point-wise one. This is normal.
 
 ## Ouest Loscolo for comparison ####
 
