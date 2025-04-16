ef4fb48f866330ebd6636f7afa7614c6819a3313
Victor Pochic <138684342+vpochic@users.noreply.github.com>
2025-04-16 16:57:09 +0200

    Update CPR script

--- NEW_CLEAN_STUFF/CPR_Dinophysis.R
+++ NEW_CLEAN_STUFF/CPR_Dinophysis.R
@@ -123,7 +123,7 @@ ggplot() +
   geom_rect(data = CPR_data_sum,
              aes(xmin = lon.bin-.25, xmax = lon.bin+.25,
                  ymin = lat.bin-.25, ymax = lat.bin+.25, # width = .5, height = .5,
-                 fill = prop_pos*log10(nsamples+1))) +
+                 fill = log10(mean_Dino+1)*prop_pos*log10(nsamples+1))) +
   scale_fill_viridis() +
   # Plotting the land
   geom_polygon(data = Worldmap, aes(x = long, y = lat, group = group), 
@@ -140,7 +140,7 @@ ggplot() +
 
 # This seems ok
 
-# ggsave('CPR_heatmap_Dino_presence.tiff', width = 164, height = 180, units = 'mm',
+# ggsave('CPR_heatmap_Dino_presence2.tiff', width = 164, height = 180, units = 'mm',
 #        dpi = 300, compression = 'lzw')
 
 ## Plotting a heatmap of sampling effort ####
@@ -148,8 +148,7 @@ ggplot() +
 # plotting the map
 
 ggplot() +
-  # Plotting the tiles of longitude and latitude depending on Dinophysis 
-  # presence
+  # Plotting the tiles of longitude and latitude depending on sampling effort
   geom_rect(data = CPR_data_sum,
             aes(xmin = lon.bin-.25, xmax = lon.bin+.25,
                 ymin = lat.bin-.25, ymax = lat.bin+.25, # width = .5, height = .5,
@@ -172,3 +171,239 @@ ggplot() +
 
 # ggsave('CPR_sampling_effort.tiff', width = 164, height = 180, units = 'mm',
 #        dpi = 300, compression = 'lzw')
+
+## Exploring the seasonality ####
+
+# First, let's just plot the month of maximum Dinophysis abundance recorded
+
+# Now, we can summarise the dataset by grouping it thanks to our new bins
+CPR_data_max <- CPR_data_binned %>%
+  # Grouping by the bin variables
+  group_by(lat.bin, lon.bin) %>%
+  # Get only rows with maximum abundance
+  top_n(1, Dinophysis_Total) %>%
+  # Filter 0 abundances
+  filter(Dinophysis_Total > 0) %>%
+  # Create a date variable in date format
+  mutate(Date = paste(Year, Month, Day, sep = '-')) %>%
+  mutate(Date = ymd(Date)) %>%
+  # Get a julian day variable
+  mutate(Julian_day = yday(Date))
+
+## Plot the month of the recorded maximum for each bin where Dinophysis was
+# observed
+
+ggplot() +
+  # Plotting the tiles of longitude and latitude depending on Dinophysis 
+  # presence
+  geom_rect(data = CPR_data_max,
+            aes(xmin = lon.bin-.25, xmax = lon.bin+.25,
+                ymin = lat.bin-.25, ymax = lat.bin+.25, # width = .5, height = .5,
+                fill = Month)) +
+  scale_fill_viridis(option = 'viridis') +
+  # Plotting the land
+  geom_polygon(data = Worldmap, aes(x = long, y = lat, group = group), 
+               fill = "grey80", color = 'gray10', linewidth = .25)+
+  coord_fixed(xlim=c(min(CPR_data$Longitude),
+                     max(CPR_data$Longitude)), 
+              ylim=c(min(CPR_data$Latitude),
+                     max(CPR_data$Latitude)))+
+  # Labels
+  labs(y = 'Latitude (degrees)', x = 'Longitude (degrees)',
+       title = "Dinophysis seasonality the NE Atlantic (CPR data)")+
+  theme_bw() +
+  theme(legend.position = 'bottom')
+
+# Doesn't seem really decisive
+
+# Let's try something else
+
+# We'll try to get the mode month (the month with the most observations) for
+# each bin
+
+# For this we define a function to calculate the mode
+find_mode <- function(x) {
+  u <- unique(x)
+  tab <- tabulate(match(x, u))
+  u[tab == max(tab)]
+}
+
+CPR_data_season <- CPR_data_binned %>%
+  # Grouping by the bin variables
+  group_by(lat.bin, lon.bin) %>%
+  # Filter only positive Dinophysis observations
+  filter(Dinophysis_Total > 0) %>%
+  # Get the mode month
+  reframe(mode_month = find_mode(Month),
+          # We also get the sum of Dinophysis observed to decide which to keep
+          # when there is more than 1 mode
+          sum_Dino = sum(Dinophysis_Total)) %>%
+  # Now we keep only the ones with the max abundance of Dino
+  group_by(lat.bin, lon.bin) %>%
+  top_n(1, sum_Dino)
+
+## Let's look at the map
+
+ggplot() +
+  # Plotting the tiles of longitude and latitude depending on Dinophysis 
+  # presence
+  geom_rect(data = CPR_data_season,
+            aes(xmin = lon.bin-.25, xmax = lon.bin+.25,
+                ymin = lat.bin-.25, ymax = lat.bin+.25, # width = .5, height = .5,
+                fill = mode_month)) +
+  scale_fill_viridis(option = 'viridis') +
+  # Plotting the land
+  geom_polygon(data = Worldmap, aes(x = long, y = lat, group = group), 
+               fill = "grey80", color = 'gray10', linewidth = .25)+
+  coord_fixed(xlim=c(min(CPR_data$Longitude),
+                     max(CPR_data$Longitude)), 
+              ylim=c(min(CPR_data$Latitude),
+                     max(CPR_data$Latitude)))+
+  # Labels
+  labs(y = 'Latitude (degrees)', x = 'Longitude (degrees)',
+       title = "Dinophysis seasonality in the NE Atlantic (CPR data)")+
+  theme_bw() +
+  theme(legend.position = 'bottom')
+
+# But there may be a bias in the monthly sampling. Let's have a look
+
+# Overall dataset
+ggplot(data = CPR_data_binned) + 
+  geom_bar(aes(x = as_factor(Month))) +
+  scale_x_discrete() +
+  theme_classic()
+
+# Seems approximately even. Is this the case across the map?
+
+CPR_data_sampling <- CPR_data_binned %>%
+  # Grouping by the bin variables
+  group_by(lat.bin, lon.bin) %>%
+  # Get the mode month
+  reframe(mode_month = find_mode(Month),
+          # We also get the sum of Dinophysis observed to decide which to keep
+          # when there is more than 1 mode (arbitrary)
+          sum_Dino = sum(Dinophysis_Total)) %>%
+  # Now we keep only the ones with the max abundance of Dino
+  group_by(lat.bin, lon.bin) %>%
+  top_n(1, sum_Dino)
+
+
+ggplot() +
+  # Plotting the tiles of longitude and latitude depending on Dinophysis 
+  # presence
+  geom_rect(data = CPR_data_sampling,
+            aes(xmin = lon.bin-.25, xmax = lon.bin+.25,
+                ymin = lat.bin-.25, ymax = lat.bin+.25, # width = .5, height = .5,
+                fill = as_factor(mode_month))) +
+  scale_fill_viridis(option = 'viridis', discrete = TRUE) +
+  # Plotting the land
+  geom_polygon(data = Worldmap, aes(x = long, y = lat, group = group), 
+               fill = "grey80", color = 'gray10', linewidth = .25)+
+  coord_fixed(xlim=c(-25,
+                     max(CPR_data$Longitude)), 
+              ylim=c(min(CPR_data$Latitude),
+                     max(CPR_data$Latitude)))+
+  # Labels
+  labs(y = 'Latitude (degrees)', x = 'Longitude (degrees)',
+       title = "CPR sampling in the NE Atlantic")+
+  theme_bw() +
+  theme(legend.position = 'bottom')
+
+### Regionalising ####
+
+# Based on the map of Dinophysis presence, we identify 4 hotspots of
+# Dinophysis in the NE Atlantic: the Iberian coast (Galicia and Portugal),
+# the Celtic seas, the Eastern North Sea (Scotland) and the Western North Sea
+# (Germany, Denmark, Norway)
+
+# We will attribute region numbers to these regions to study more specifically
+# Dinophysis in each of them.
+
+CPR_data_regions <- CPR_data_binned %>%
+  # Define regions
+  mutate(region = ifelse(
+    # Iberian Coast (#1)
+    lat.bin <= 44.5 & lat.bin >= 36.5 & lon.bin <= -8.5 & lon.bin >= -11,
+    1,
+    ifelse(
+      # Celtic seas (#2)
+      lat.bin <= 53 & lat.bin >= 45 & lon.bin <= -1 & lon.bin >= -10.5,
+      2,
+      ifelse(
+        # Western North Sea (#3)
+        lat.bin <= 60 & lat.bin >= 52.5 & lon.bin < 3.5 & lon.bin >= -3.5,
+        3,
+        ifelse(
+          # Eastern North Sea (#4)
+          lat.bin <= 60 & lat.bin >= 52.5 & lon.bin <= 14.5 & lon.bin >= 3.5,
+          4,
+          # Else, #0
+          0))))) %>%
+  # Create an appropriate date variable
+  mutate(Date = paste(Year, Month, Day, sep = '-')) %>%
+  mutate(Date = ymd(Date)) %>%
+  # Get a julian day variable
+  mutate(Julian_day = yday(Date))
+
+# Do the regions correspond to what we want?
+ggplot() +
+  # Plotting the tiles of longitude and latitude depending on the region
+  geom_rect(data = CPR_data_regions,
+            aes(xmin = lon.bin-.25, xmax = lon.bin+.25,
+                ymin = lat.bin-.25, ymax = lat.bin+.25, # width = .5, height = .5,
+                fill = as_factor(region))) +
+  scale_fill_discrete() +
+  # Plotting the land
+  geom_polygon(data = Worldmap, aes(x = long, y = lat, group = group), 
+               fill = "grey80", color = 'gray10', linewidth = .25)+
+  coord_fixed(xlim=c(-25,
+                     max(CPR_data$Longitude)), 
+              ylim=c(min(CPR_data$Latitude),
+                     max(CPR_data$Latitude)))+
+  # Labels
+  labs(y = 'Latitude (degrees)', x = 'Longitude (degrees)',
+       title = "Regionalization of the NE Atlantic")+
+  theme_bw() +
+  theme(legend.position = 'bottom')
+
+# We can also plot the regions as polygons on top of the Dinophysis presence map
+
+# For this, we import a tibble containing the necessary information to provide to
+# geom_polygon
+
+regions_polygon <- read.csv2('Data/CPR/CPR_regions_polygon.csv', header = TRUE,
+                             fileEncoding = 'ISO-8859-1')
+
+ggplot() +
+  # Plotting the tiles of longitude and latitude depending on Dinophysis presence
+  geom_rect(data = CPR_data_sum,
+            aes(xmin = lon.bin-.25, xmax = lon.bin+.25,
+                ymin = lat.bin-.25, ymax = lat.bin+.25, # width = .5, height = .5,
+                fill = log10(mean_Dino+1)*prop_pos*log10(nsamples+1))) +
+  scale_fill_viridis() +
+  # Plotting the land
+  geom_polygon(data = Worldmap, aes(x = long, y = lat, group = group), 
+               fill = "grey80", color = 'gray10', linewidth = .25)+
+  # Plotting the limits of the regions
+  geom_polygon(data = regions_polygon, aes(x = lon, y = lat, group = region,
+                                           color = as_factor(region)), 
+               fill = "transparent", linewidth = 1)+
+  scale_color_discrete() +
+  # Limits of the map
+  coord_fixed(xlim=c(-25,
+                     max(CPR_data$Longitude)), 
+              ylim=c(min(CPR_data$Latitude),
+                     max(CPR_data$Latitude)))+
+  # Labels
+  labs(y = 'Latitude (degrees)', x = 'Longitude (degrees)',
+       title = "Dinophysis in the NE Atlantic (CPR data)")+
+  theme_bw() +
+  theme(legend.position = 'bottom')
+
+# Plot the seasonality of Dinophysis depending on the region
+ggplot(data = CPR_data_regions) +
+  geom_point(aes(x = Julian_day, y = Dinophysis_Total,
+                 color = as_factor(region))) +
+  facet_wrap(facets = 'region', scales = 'free_y') +
+  scale_color_discrete(guide = 'none') +
+  theme_classic()
